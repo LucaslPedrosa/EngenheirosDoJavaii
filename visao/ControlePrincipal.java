1137365bbf8bf0d17e3f17e47425bf89466fc485
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2021-11-09 21:00:58 -0300

    Atualizacao
    Botao de exit funcionando não iterativo

--- visao/ControlePrincipal.java
+++ visao/ControlePrincipal.java
@@ -1,6 +1,38 @@
 package visao;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControlePrincipal {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class ControlePrincipal implements Initializable{
+
+    @FXML
+    private ImageView exitButton;
+
+    @FXML
+    private AnchorPane AnchorPai;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+        exitButton.setOnMouseClicked(Event ->{
+            Stage stage;
+            stage = (Stage)AnchorPai.getScene().getWindow();
+            stage.close();
+        });
+
+        exitButton.setOnMouseEntered(Event ->{
+            
+        });
+        
+    }
+
 
 }
