20dd75c74ba485270accfd2a1b25a4dd11fe4585
RogerDanielSS <91351818+RogerDanielSS@users.noreply.github.com>
2021-11-18 10:00:37 -0300

    a

--- controle/VacinanteControle.java
+++ controle/VacinanteControle.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import modelo.ProfSaude;
 import modelo.Vacina;
 import modelo.Vacinacao;
 import modelo.Vacinante;
